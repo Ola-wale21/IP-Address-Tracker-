e309f9f4828427f9d793e9d78084ed1bac0a09f3
Olakunle Samuel <olakunleolawale21@gmail.com>
2025-12-15 12:14:26 +0100

    vercel add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/IP-Address-Tracker/",
+  base:  process.env.VITE_BASE_PATH || "/IP-Address-Tracker/",
   plugins: [react()],
 })
